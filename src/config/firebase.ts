09b75a73e980ec088014d91e27eec9a7b72019a2
franly2 <franlybudipramana588@gmail.com>
2026-03-02 17:54:39 +0700

    fix firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDEDlcHeVXqGyK-x5jLu-3FzVYT5awmTEE",
-  authDomain: "react-social-media-7b56c.firebaseapp.com",
-  projectId: "react-social-media-7b56c",
-  storageBucket: "react-social-media-7b56c.appspot.com",
-  messagingSenderId: "850317575754",
-  appId: "1:850317575754:web:116a4bb04ecd90a103c8b3",
-  measurementId: "G-XSXE6ELPQY",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
